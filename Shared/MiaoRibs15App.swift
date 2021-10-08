f4a0a0bb3fa87f91b268da65e747a122d033ff3a
Mia <mia.yu@taiwantaxi.com.tw>
2021-10-08 10:46:10 +0800

    Initial Commit

---
+++ Shared/MiaoRibs15App.swift
@@ -0,0 +1,17 @@
+//
+//  MiaoRibs15App.swift
+//  Shared
+//
+//  Created by Mia on 2021/10/8.
+//
+
+import SwiftUI
+
+@main
+struct MiaoRibs15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
